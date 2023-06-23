5e6434314f861d4ad22d702af12bc6ec8ba1d489
LorenzoStigliano <stiglianolorenzo@gmail.com>
2023-06-23 14:35:58 +0100

    added model ensmable with lsp mathcing

---
+++ src/main_lsp_ensamble.py
@@ -0,0 +1,62 @@
+import argparse
+import random
+import numpy as np
+import torch
+
+from models.model_config import * 
+from trainers.lsp_ensamble_3_trainer import cross_validation_3
+
+from utils.builders import new_folder
+from utils.loaders import load_data
+
+import joblib
+from joblib import Parallel, delayed
+
+def train_main_model(dataset, model, view, cv_number, model_args, run=0):
+    
+    torch.manual_seed(run)
+    np.random.seed(run)
+    random.seed(run)   
+
+    cv_name = str(cv_number)+"Fold"
+    model_name = "MainModel_"+cv_name+"_"+dataset+"_"+model_args["model_name"]
+
+    G_list = load_data(dataset, view, NormalizeInputGraphs=False)
+
+    new_folder(model_args["model_name"], model_args["evaluation_method"])
+    
+    if gcn_args["evaluation_method"] == "model_assessment":
+            model_name += f"_run_{run}_fixed_init"
+    
+    if model_args["model_name"] == "gcn_student_lsp_ensamble_3":
+        cross_validation_3(model_args, G_list, view, model_name, cv_number, n_students=3, run=run)
+
+def parrallel_run(run):
+    print(run)
+    datasets_asdnc = ['gender_data']
+    views = [0, 2, 4, 5] #0, 2, 4, 5
+    for dataset_i in datasets_asdnc:
+        for view_i in views:
+            models = [gcn_student_lsp_ensamble_3_args] 
+            for model in models:
+                for cv in [3, 5, 10]:
+                    train_main_model(dataset_i, model["model_name"], view_i, cv, model, run)
+
+if __name__ == '__main__':
+        
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', type=str, default='train', choices=['train', 'results'])
+    parser.add_argument('--runs', nargs='+', help='Enter a list of seeds for the runs: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9', default=[0])
+    parser.add_argument('--n_jobs', type=int, help='parrallel jobs', default=1)
+
+    args = parser.parse_args()
+    runs = [int(run) for run in args.runs]
+    n_jobs = int(args.n_jobs)
+
+    if args.mode == 'train':
+        '''
+        Training GNN Models with datasets of data directory.
+        '''
+        runs = runs # 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+        #joblib.cpu_count()
+        Parallel(n_jobs=n_jobs)(delayed(parrallel_run)(run) for run in runs)
--- src/models/model_config.py
+++ src/models/model_config.py
@@ -54,6 +54,23 @@ gcn_student_ensamble_2_args = {
     "n_students":2 # TOTAL number of students in ensamble 
 }
 
+gcn_student_lsp_ensamble_3_args = {
+    "num_epochs":1, 
+    "lr": 0.0001, # 0.0001 when training without teacher
+    "weight_decay":5e-4, 
+    "hidden_dim":64,
+    "dropout":0,
+    "threshold":"median", # Threshold the graph adjacency matrix. Possible values: no_threshold, median, mean
+    "model_name":"gcn_student_lsp_ensamble_3",
+    "evaluation_method": "model_assessment", # model selection or model assessment
+    "alpha": 1, # ensamble ce loss
+    "beta": 2,  # ensamble kd loss
+    "gamma": 2, # sum of student ce loss
+    "lambda":1, # disentanglement loss
+    "T": 3, #Temperature parameter for soft logit target 
+    "n_students":3 # TOTAL number of students in ensamble 
+}
+
 gcn_student_ensamble_3_args = {
     "num_epochs":50, 
     "lr": 0.001, # 0.0001 when training without teacher
